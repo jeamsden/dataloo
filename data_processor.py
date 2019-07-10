52ec62eb6a9e83a778cf4d2e57958be3506bb22b
jeamsden <amsden@outlook.com>
2019-07-09 22:30:47 -0400

    Date/time processing performance improvements.

--- data_processor.py
+++ data_processor.py
@@ -14,25 +14,24 @@ def get_counter_data(source='local'):
         cow_counter_readings = pd.read_csv('https://opendata.arcgis.com/datasets/5d41afff252e45b5b5fe7fc3fd5df3ab_0.csv')
         #COK data
         cok_counter_readings = pd.read_csv('https://app2.kitchener.ca/appdocs/opendata/staticdatasets/Trails_Counters_Pedestrians_Cyclists.csv')
-    
+    print('I made it here quickly. FNF')
     cok_counter_info = cok_counter_readings.groupby(by=['STATION_LOCATION_DESCRIPTION', 'X_COORD_LL_DD', 'Y_COORD_LL_DD']).first().reset_index()[['STATION_LOCATION_DESCRIPTION', 'X_COORD_LL_DD', 'Y_COORD_LL_DD']]
     cok_counter_info = cok_counter_info.rename(columns={'STATION_LOCATION_DESCRIPTION': 'LOCATION', 'X_COORD_LL_DD': 'LONG', 'Y_COORD_LL_DD': 'LAT'})
-    
+    print('I made it here quickly. HFV')
     cok_counter_info['ID'] = cok_counter_info['LOCATION']
     cok_counter_info['LOCATION'] = cok_counter_info['LOCATION'].replace('CHERRY ST', 'Iron Horse/Trans Canada Trail at Cherry St. S.')
     cok_counter_info['LOCATION'] = cok_counter_info['LOCATION'].replace('BORDEN AVE S', 'Iron Horse/Trans Canada Trail at Borden St. S.')
     cok_counter_info['LOCATION'] = cok_counter_info['LOCATION'].replace('QUEEN ST S', 'Iron Horse/Trans Canada Trail at Queen St. S.')
-
+    print('I made it here quickly. BUJ')
     cow_counter_info = cow_counter_info[['LOCATION', 'LONG', 'LAT', 'ID']]
-
-
+    print('I made it here quickly. EDT')
     counter_info = pd.concat([cow_counter_info, cok_counter_info]).reset_index(drop=True)
-
+    print('I made it here quickly. JKY')
     cok_counter_readings = cok_counter_readings.rename(columns={'DATE_TIME': 'DATE', 'STATION_LOCATION_DESCRIPTION': 'LOC_ID', 'PEDESTRIANS': 'PEDESTRIAN_TOTAL', 'CYCLISTS': 'CYCLIST_TOTAL', 'TOTAL': 'TOTAL_COUNT',})
-
-    cok_counter_readings['DATE'] = pd.to_datetime(cok_counter_readings['DATE']).dt.date
-    cow_counter_readings['DATE'] = pd.to_datetime(cow_counter_readings['DATE']).dt.date
-
+    print('I made it here quickly. SKV')
+    cok_counter_readings['DATE'] = pd.to_datetime(cok_counter_readings['DATE'], infer_datetime_format=True).dt.date
+    cow_counter_readings['DATE'] = pd.to_datetime(cow_counter_readings['DATE'], infer_datetime_format=True).dt.date
+    print('I made it here quickly. GFB')
     cok_counter_readings = cok_counter_readings[['LOC_ID', 'DATE', 'TOTAL_COUNT', 'PEDESTRIAN_TOTAL', 'CYCLIST_TOTAL']]
     
     cow_counter_readings = cow_counter_readings[['LOC_ID', 'DATE', 'TOTAL_COUNT', 'PEDESTRIAN_TOTAL', 'CYCLIST_TOTAL']]
