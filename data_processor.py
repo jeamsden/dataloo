49b65a8610b7968e1d0e93fa47f2da25abe459db
jeamsden <amsden@outlook.com>
2019-08-02 23:35:41 -0400

    Use open data portal data sources.

--- data_processor.py
+++ data_processor.py
@@ -4,8 +4,8 @@ datasets = {
     'cow_counters': {
         'type': 'simple',
         'steps': [
-            #'''df = pd.read_csv('https://opendata.arcgis.com/datasets/a5e1adba2e5545a9b4f0a1d198cd0498_0.csv')''',
-            '''df = pd.read_csv('EcoCounters.csv')''',
+            '''df = pd.read_csv('https://opendata.arcgis.com/datasets/a5e1adba2e5545a9b4f0a1d198cd0498_0.csv')''',
+            #'''df = pd.read_csv('EcoCounters.csv')''',
             '''df = df[['LOCATION', 'LONG', 'LAT', 'ID']]'''
         ],
         'df': None
@@ -13,8 +13,8 @@ datasets = {
     'cok_counters': {
         'type': 'simple',
         'steps': [
-            #'''df = pd.read_csv('https://app2.kitchener.ca/appdocs/opendata/staticdatasets/Trails_Counters_Pedestrians_Cyclists.csv')''',
-            '''df = pd.read_csv('Trails_Counters_Pedestrians_Cyclists.csv')''',
+            '''df = pd.read_csv('https://app2.kitchener.ca/appdocs/opendata/staticdatasets/Trails_Counters_Pedestrians_Cyclists.csv')''',
+            #'''df = pd.read_csv('Trails_Counters_Pedestrians_Cyclists.csv')''',
             '''df = df.groupby(by=['STATION_LOCATION_DESCRIPTION', 'X_COORD_LL_DD', 'Y_COORD_LL_DD']).first().reset_index()[['STATION_LOCATION_DESCRIPTION', 'X_COORD_LL_DD', 'Y_COORD_LL_DD']]''',
             '''df = df.rename(columns={'STATION_LOCATION_DESCRIPTION': 'LOCATION', 'X_COORD_LL_DD': 'LONG', 'Y_COORD_LL_DD': 'LAT'})''',
             '''df['ID'] = df['LOCATION']''',
@@ -30,8 +30,8 @@ datasets = {
     'cow_counter_readings': {
         'type': 'simple',
         'steps': [
-            #'''df = pd.read_csv('https://opendata.arcgis.com/datasets/5d41afff252e45b5b5fe7fc3fd5df3ab_0.csv')''',
-            '''df = pd.read_csv('City_of_Waterloo_Trail_Counter_Data.csv')''',
+            '''df = pd.read_csv('https://opendata.arcgis.com/datasets/5d41afff252e45b5b5fe7fc3fd5df3ab_0.csv')''',
+            #'''df = pd.read_csv('City_of_Waterloo_Trail_Counter_Data.csv')''',
             '''df['DATE'] = pd.to_datetime(df['DATE'], infer_datetime_format=True).dt.date''',
             '''df = df[['LOC_ID', 'DATE', 'TOTAL_COUNT', 'PEDESTRIAN_TOTAL', 'CYCLIST_TOTAL']]''',
             '''df = df.groupby(by=['LOC_ID', 'DATE']).sum().reset_index()'''
@@ -41,8 +41,8 @@ datasets = {
     'cok_counter_readings': {
         'type': 'simple',
         'steps': [
-            #'''df = pd.read_csv('https://app2.kitchener.ca/appdocs/opendata/staticdatasets/Trails_Counters_Pedestrians_Cyclists.csv')''',
-            '''df = pd.read_csv('Trails_Counters_Pedestrians_Cyclists.csv')''',
+            '''df = pd.read_csv('https://app2.kitchener.ca/appdocs/opendata/staticdatasets/Trails_Counters_Pedestrians_Cyclists.csv')''',
+            #'''df = pd.read_csv('Trails_Counters_Pedestrians_Cyclists.csv')''',
             '''df = df.rename(columns={'DATE_TIME': 'DATE', 'STATION_LOCATION_DESCRIPTION': 'LOC_ID', 'PEDESTRIANS': 'PEDESTRIAN_TOTAL', 'CYCLISTS': 'CYCLIST_TOTAL', 'TOTAL': 'TOTAL_COUNT',})''',
             '''df['DATE'] = pd.to_datetime(df['DATE'], infer_datetime_format=True).dt.date''',
             '''df = df[['LOC_ID', 'DATE', 'TOTAL_COUNT', 'PEDESTRIAN_TOTAL', 'CYCLIST_TOTAL']]''',
