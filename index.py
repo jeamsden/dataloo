0d3aef2d089e7e70891d854cab8a0e322524d811
jeamsden <amsden@outlook.com>
2019-08-03 11:45:54 -0400

    Fix heroku

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ from app import app
 import layouts
 import callbacks
 
-#server=app.server
+# This line seems to be needed by heroku
+server=app.server
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
